93177eb1946beb84cc32d0d984c2a8b0424e3f25
ilya loginov <ilya.loginov@exactprosystems.com>
2020-08-12 13:25:28 +0400

    [JIRA] TH2-508. Fix error with EmptyAdapter

--- src/main/java/com/exactpro/th2/simulator/adapter/EmptyAdapter.java
+++ src/main/java/com/exactpro/th2/simulator/adapter/EmptyAdapter.java
@@ -1,11 +1,24 @@
+/*******************************************************************************
+ * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package com.exactpro.th2.simulator.adapter;
 
 import java.io.IOException;
 
 import org.jetbrains.annotations.NotNull;
 
-import com.exactpro.th2.infra.grpc.ConnectionID;
 import com.exactpro.th2.configuration.MicroserviceConfiguration;
+import com.exactpro.th2.infra.grpc.ConnectionID;
 import com.exactpro.th2.simulator.IAdapter;
 import com.exactpro.th2.simulator.ISimulator;
 
@@ -13,9 +26,10 @@ import com.exactpro.th2.simulator.ISimulator;
  * Implementation of {@link IAdapter}. Does nothing.
  */
 public class EmptyAdapter implements IAdapter {
-    @Override
-    public void init(@NotNull MicroserviceConfiguration configuration, @NotNull ConnectionID connectionID, @NotNull ISimulator simulator) {}
 
     @Override
     public void close() throws IOException {}
+
+    @Override
+    public void init(@NotNull MicroserviceConfiguration configuration, @NotNull ConnectionID connectionID, boolean parseBatch, @NotNull ISimulator simulator) { }
 }
