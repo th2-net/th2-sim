35a12c30dec42207a665891ee6d2b20f6d62f60d
Nikita Shaposhnikov <topru123@gmail.com>
2021-11-30 17:00:30 +0300

    Test fixtures (#29)
    * copyright update
    
    * testFixtures as utils for rule testing and review changes
    
    * replaced to kotlin realization of regex
    
    * speedUp parameter was implemented
    
    * doc
    
    * split to two methods assertTriggered and handle, default timestamp
    
    * additional call for assertion of trigger and handle after
    
    * doc for assertAndHandle
    
    * doc refactoring, bigDecimal and long assertion of value, closeable test class
    
    * fail message refactoring and doc fixes
    
    * null check message fix and private handle method
    
    * removed redundant TestUtils.kt

--- src/main/java/com/exactpro/th2/sim/rule/IRuleContext.java
+++ src/main/java/com/exactpro/th2/sim/rule/IRuleContext.java
@@ -1,15 +1,18 @@
-/*******************************************************************************
- * Copyright 2021-2021 Exactpro (Exactpro Systems Limited)
+/*
+ * Copyright 2020-2021 Exactpro (Exactpro Systems Limited)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
 
 package com.exactpro.th2.sim.rule;
 
@@ -24,22 +27,51 @@ import com.exactpro.th2.sim.rule.action.IAction;
 import com.exactpro.th2.sim.rule.action.ICancellable;
 
 public interface IRuleContext {
+
+    /**
+     * Attempts to send a msg immediately
+     */
     void send(@NotNull Message msg);
 
+    /**
+     * Attempts to send a batch immediately
+     */
     void send(@NotNull MessageBatch batch);
 
+    /**
+     * Attempts to send a msg after a specified delay
+     */
     void send(@NotNull Message msg, long delay, TimeUnit timeUnit);
 
+    /**
+     * Attempts to send a batch after a specified delay
+     */
     void send(@NotNull MessageBatch batch, long delay, TimeUnit timeUnit);
 
+    /**
+     * Attempts to execute action immediately
+     * @return an entity which can be used cancel this operation
+     */
     ICancellable execute(@NotNull IAction action);
 
+    /**
+     * Attempts to execute action after a specified delay
+     * @return an entity which can be used cancel this operation
+     */
     ICancellable execute(long delay, @NotNull IAction action);
 
+    /**
+     * Attempts to execute action after a specified delay and
+     * then periodically using a specified period
+     * @return an entity which can be used cancel this operation
+     */
     ICancellable execute(long delay, long period, @NotNull IAction action);
 
     String getRootEventId();
 
+    /**
+     * Attempts to send an event immediately
+     */
     void sendEvent(Event event);
 
     void removeRule();
